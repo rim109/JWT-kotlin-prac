8efbdd84902100928e0006d23e01c5180abd15e8
rim109 <fla7722@naver.com>
2024-01-16 15:20:13 +0900

    first commit

---
+++ src/main/kotlin/com/example/jwtprac/JwtPracApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jwtprac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtPracApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtPracApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jwtprac/JwtPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jwtprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtPracApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
